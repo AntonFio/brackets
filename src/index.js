a2a89a0af13bb3e62fdbf64cd41cf2f3491cef45
AntonFio <anton_fiolkovski@mail.ru>
2022-08-12 16:42:15 +0300

    fix: fix file

--- src/index.js
+++ src/index.js
@@ -1,38 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-   const open = ['(', '{', '[', '|'];
+   
+
+   const open = ['(', '{', '[', '||'];
    const end = {
      [')']: '(',
       ['}']: '{',
       [']']: '[',
       ['|']: '|',
    };
-
+console.log(str.length)
    function isBracketsOk(strAll) {
       let stack = [];
   
       for (let i = 0; i < strAll.length; i++) {
-        let item = strAll[i];
+         let item = strAll[i];
         
         if (open.includes(item)) {
-          stack.push(item);
-        } else {
-          if (stack.length === 0) {
-            return false;
-          }
-  
+           stack.push(item);
+         } else {
           let topElement = stack[stack.length - 1];
-  
-          if (end[item] === topElement) {
-            stack.pop();
-          } else {
-            return false;
+           if (end[item] === topElement) {
+              stack.pop();
           }
         }
       }
   
-      return stack.length === 0;
+     return stack.length === 0
    }
    return isBracketsOk(str)
 }
-
 
