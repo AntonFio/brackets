2d03252e965b7412f075730dc562cc855ae5d313
AntonFio <anton_fiolkovski@mail.ru>
2022-08-12 14:00:11 +0300

    fix: file

--- src/index.js
+++ src/index.js
@@ -1,32 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  const OPEN_BRACKETS = ['(', '{'];
-  const BRACKETS_PAIR = {
-     [')']: '(',
-    ['[']: ']',
- 
-  };
+   const arrOpen = ['(', '[', '{', '||'];
+   const close = {
+      [')']: '(',
+      [']']: '[',
+      ['}']: '{',
+      ['|']: '|',
+   }
 
-let stack = [];
+let arr = [];
 
-for (let i = 0; i < str.length; i++) {
-  let currentSymbol = str[i];
-   
-  if (OPEN_BRACKETS.includes(currentSymbol)) {
-     stack.push(currentSymbol);
-  } else {
-     if (stack.length === 0) {
-        return false;
-     }
-
-     let topElement = stack[stack.length - 1];
-
-     if (BRACKETS_PAIR[currentSymbol] === topElement) {
-        stack.pop();
-     } else {
-        return false;
-     }
-  }
+for (let i = 0; i < str.length; i++){
+   let strItem = str[i];
+   if (arrOpen.includes(strItem)) {
+      arr.push(strItem)
+   }
+   let topElement = arr[arr.length - 1];
+   if (close[strItem] === topElement) {
+      arr.pop()
+   } 
+   return arr.length === 0;
 }
-return stack.length === 0;
 
 }
+
+
